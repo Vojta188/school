f47fbe547138d17ecbc825029c36a5f4cce95283
Vojtěch <75996784+Vojta188@users.noreply.github.com>
2024-10-24 12:29:30 +0200

    cyklus

---
+++ Cyklus.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int vstup;
+	int i;
+	
+	cin >> vstup;
+	int c = vstup;
+	
+
+	for (i = 0; i < vstup; i++)
+	{
+		
+		
+		if (i > (vstup / 2)) {
+			c = i + 1;
+			
+		}
+		else {
+			c = c - i;
+		}
+		
+		cout << " c " << c;
+		cout << " i " << i;
+		cout << " vstup " << (vstup / 2);
+		
+		while(vstup > c)
+		{
+			
+			
+				cout << " ";
+				c++;
+			
+			
+		}
+		
+		cout << "x" << "\n";
+		
+		
+		
+		
+	}
